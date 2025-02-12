0a5e48045fc3e4dad5482fff13ed7686f56c3893
Jmlock <jmgcpalma7@gmail.com>
2025-02-12 21:17:01 +0100

    Configurar BD en Render

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -12,7 +12,7 @@ export function query(text: string, params?: any[]): Promise<any> {
   return pool.query(text, params);
 } */
 
-const connectionString = 'postgresql://dbuser:pXcMlGV6Q2mAXTTzTH91HIFOvEyHhw0w@dpg-cumd0r1u0jms73bo2vt0-a.frankfurt-postgres.render.com/dragonball_jjzi';
+const connectionString = 'postgresql://dbuser:pXcMlGV6Q2mAXTTzTH91HIFOvEyHhw0w@dpg-cumd0r1u0jms73bo2vt0-a/dragonball_jjzi';
 const pool = new Pool({ connectionString });
 export function query(text: any, params?: any[]) {
   return pool.query(text, params);
