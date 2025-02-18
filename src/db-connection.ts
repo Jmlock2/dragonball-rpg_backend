659a0158bbeba80c1f49a7012fb97022da4f96bd
Jmlock <jmgcpalma7@gmail.com>
2025-02-18 21:56:37 +0100

    18-02-2025

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -13,7 +13,11 @@ export function query(text: string, params?: any[]): Promise<any> {
 } */
 
 const connectionString = 'postgresql://zdbuser:FivsjMchmvLt5ZE1RmrfL2uiX2Yk364u@dpg-cuplf18gph6c73cq3k1g-a.frankfurt-postgres.render.com/dragonballrpg';
-const pool = new Pool({ connectionString });
+const pool = new Pool({
+  connectionString, ssl: { rejectUnauthorized: false }
+});
 export function query(text: any, params?: any[]) {
   return pool.query(text, params);
 }
+
+export default pool;
--- src/index.ts
+++ src/index.ts
@@ -11,84 +11,43 @@ app.use(cors());
 
 app.use(jsonParser);
 
-// Endpoint GET para obtener el registro de los usuarios
-app.get('/usuarios', async (req, res) => {
-    try {
-        let rpg_response = await db.query('SELECT * FROM usuarios;');
-        res.status(200).json(rpg_response.rows);
-    } catch (error) {
-        console.error('Error al obtener usuarios:', error);
-        res.status(500).json({ error: 'Error al obtener usuarios' });
-    }
-});
-
-// Endpoint POST para insertar un nuevo usuario
+// Endpoint para registrar un nuevo usuario en la base de datos
 app.post('/usuarios', async (req, res) => {
-    console.log('Datos recibidos', req.body)
     try {
-        let { name, email } = req.body; // Obtiene el nombre y email del cuerpo de la solicitud
+        let { auth0_id, name } = req.body;
 
-        // Valida que los datos no estén vacíos
-        if (!name || !email) {
-            return res.status(400).json({ error: 'El nombre y el email son obligatorios.' });
+        if (!auth0_id) {
+            return res.status(400).json({ error: 'El ID de Auth0 es obligatorio' });
         }
 
-        // Inserta un nuevo usuario en la base de datos
-        let result = await db.query(
-            'INSERT INTO usuarios (name, email) VALUES ($1, $2) RETURNING *;',
-            [req.body.name, req.body.email]
-        );
+        // Verificar si el usuario ya existe en la base de datos
+        let usuarioExistente = await db.query('SELECT * FROM users WHERE auth0_id = $1', [auth0_id]);
 
-        console.log('Usuario guardado', result.rows[0]) // Log de usuario
-        res.status(201).json(result.rows[0]); // Devuelve el usuario recién creado
-    } catch (error) {
-        console.error('Error al insertar usuario:', error);
-        res.status(500).json({ error: 'Error al insertar usuario.' });
-    }
-});
-
-// Endpoint DELETE para eliminar un usuario
-app.delete('/usuarios/:id', async (req, res) => {
-    try {
-        let { id } = req.params; // Obtiene el id del usuario desde los parámetros de la URL
-
-        // Intenta eliminar el usuario
-        let result = await db.query('DELETE FROM usuarios WHERE id = $1 RETURNING *;', [id]);
-
-        if (result.rowCount === 0) {
-            return res.status(404).json({ error: 'Usuario no encontrado.' });
+        if (usuarioExistente.rows.length > 0) {
+            return res.status(400).json({ error: 'El usuario ya está registrado' });
         }
 
-        res.status(200).json({ message: 'Usuario eliminado correctamente.', usuario: result.rows[0] });
+        let nuevoUsuario = await db.query(
+            'INSERT INTO users (auth0_id, name) VALUES ($1, $2) RETURNING id',
+            [auth0_id, name || 'Jugador']
+          );
+          
+          let userId = (nuevoUsuario.rows as any[])[0].id;
+
+        if (!userId) {
+            return res.status(500).json({ error: 'Error al registrar el usuario.' });
+        }
+
+        // Crear un registro en tabla de ranking con score inicial de 0
+        await db.query('INSERT INTO ranking (user_id, score) VALUES ($1, 0)', [userId]);
+
+        res.status(201).json({ message: 'Usuario registrado correctamente.' });
+
     } catch (error) {
-        console.error('Error al eliminar usuario:', error);
-        res.status(500).json({ error: 'Error al eliminar usuario.' });
+        console.error('Error al registrar el usuario:', error);
+        res.status(500).json({ error: 'Error al registrar el usuario.' });
     }
 });
-
-// Endpoint PUT para actualizar el rank de un usuario
-app.put('/actualizar-ranking', async (req, res) => {
-    const { email, puntos } = req.body;
-  
-    try {
-      await db.query('UPDATE usuarios SET ranking = ranking + $1 WHERE email = $2;', [puntos, email]);
-      res.status(200).json({ message: 'Ranking actualizado correctamente' });
-    } catch (error) {
-      console.error('Error al actualizar el ranking:', error);
-      res.status(500).json({ error: 'Error al actualizar el ranking' });
-    }
-  });
-
-// Endpoint para obtener el ranking de jugadores (ordenado por puntos)
-app.get('/ranking', async (req, res) => {
-    try {
-      let result = await db.query('SELECT name, ranking FROM usuarios ORDER BY ranking DESC;');
-      res.status(200).json(result.rows); // Devolver el ranking en formato JSON
-    } catch (error) {
-      console.error('Error al obtener el ranking:', error);
-      res.status(500).json({ error: 'Error al obtener el ranking' });
-    }
-  });
 
 
 const port = process.env.PORT || 10000;
