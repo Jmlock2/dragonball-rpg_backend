0d1439aae6e2e65f9e96e09dd0a7b44564525293
Jmlock <jmgcpalma7@gmail.com>
2025-02-19 22:48:56 +0100

    22:49

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,17 +1,5 @@
 import { Pool } from 'pg';
 
-/* const pool = new Pool({
-  user: 'postgres',
-  password: '1234',
-  host: 'localhost',
-  port: 5432, // default Postgres port
-  database: 'dragonball'
-});
-
-export function query(text: string, params?: any[]): Promise<any> {
-  return pool.query(text, params);
-} */
-
 const connectionString = 'postgresql://zdbuser:FivsjMchmvLt5ZE1RmrfL2uiX2Yk364u@dpg-cuplf18gph6c73cq3k1g-a.frankfurt-postgres.render.com/dragonballrpg';
 const pool = new Pool({
   connectionString, ssl: { rejectUnauthorized: false }
--- src/index.ts
+++ src/index.ts
@@ -94,29 +94,3 @@ app.put('/ranking', async (req, res) => {
 const port = process.env.PORT || 10000;
 
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
-
-
-// Antiguo ENDPOINT para registrar un nuevo usuario en la base de datos
-/* app.post('/register', async (req, res) => {
-    try {
-        let { auth0_id, name } = req.body;
-
-        // 1️⃣ Verificar si el usuario ya existe en la base de datos
-        let usuarioExistente = await pool.query('SELECT * FROM users WHERE auth0_id = $1', [auth0_id]);
-
-        if (usuarioExistente.rows.length > 0) {
-            return res.status(200).json({ message: "Usuario ya está registrado" }); // Devuelve mensaje sin error
-        }
-
-        // 2️⃣ Si no existe, lo registramos
-        let nuevoUsuario = await pool.query(
-            'INSERT INTO users (auth0_id, name) VALUES ($1, $2) RETURNING *',
-            [auth0_id, name]
-        );
-
-        res.status(201).json(nuevoUsuario.rows[0]); // Devolvemos el usuario creado
-    } catch (error) {
-        console.error('Error al registrar usuario:', error);
-        res.status(500).json({ message: 'Error interno del servidor' });
-    }
-}); */
